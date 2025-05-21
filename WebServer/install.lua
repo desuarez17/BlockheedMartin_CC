7a2731216498791df5d3279f828ffb5ca8fc13d2
Damian S. <70759726+desuarez17@users.noreply.github.com>
2025-05-20 21:17:49 -0400

    dynamic sourse

--- WebServer/install.lua
+++ WebServer/install.lua
@@ -1,3 +1,5 @@
+--wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/install.lua install.lua
+sourse = "main"
 print("Installing Webserver Program...")
 
 print("Deleting old files...")
@@ -7,21 +9,21 @@ shell.run("delete /*")
 print("Downloading new files...")
 
 --main programs
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/webServer.lua webServer.lua")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/startup.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/webServer.lua webServer.lua")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/ma"..sourse.."in/WebServer/startup.lua startup.lua")
 
 --pages
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/BlockheedMartinNet.txt BlockheedMartinNet.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/Catalog.txt Catalog.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/Service.txt Service.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/Mission.txt Mission.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/About.txt About.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/ABT_CrabbyWings15.txt ABT_CrabbyWings15.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/ABT_desuarez17.txt ABT_desuarez17.txt")
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/404.txt 404.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/BlockheedMartinNet.txt BlockheedMartinNet.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/Catalog.txt Catalog.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/Service.txt Service.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/Mission.txt Mission.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/About.txt About.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/ABT_CrabbyWings15.txt ABT_CrabbyWings15.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/ABT_desuarez17.txt ABT_desuarez17.txt")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/404.txt 404.txt")
 
 --installer
-shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/main/WebServer/install.lua install.lua")
+shell.run("wget https://raw.githubusercontent.com/desuarez17/BlockheedMartin_CC/refs/heads/"..sourse.."/WebServer/install.lua install.lua")
 
 print("Downloading complete.")
 print("Restarting Webserver...")
